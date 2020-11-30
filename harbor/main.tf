be83b689b0e80169a037f55c277c0b4034e748ca
langoureaux-s <sebastien.langoureaux@harmonie-mutuelle.fr>
2020-11-30 14:19:53 +0000

    Fix app module

--- harbor/main.tf
+++ harbor/main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 0.12.0"
+  required_version = ">= 0.13.0"
 
   # Live modules pin exact provider version; generic modules let consumers pin the version.
   required_providers {
